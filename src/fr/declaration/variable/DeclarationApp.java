290f44ba6d761315b7cec4aaec8b507b0165a2a5
melheb.younes <melheb.younes@gmail.com>
2021-04-22 12:56:50 +0200

    Declare the variable & display it

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -2,6 +2,20 @@ package fr.declaration.variable;
 
 public class DeclarationApp {
 	public static void main(String[] args) {
-		
+		byte b = 10;
+		short s = -17282;
+		int i = 2845325;
+		long l = 10000L;
+		float f = 2.5F;
+		double d = 6.4D;
+		char c = 'c';
+		boolean myBool = true;
+
+		System.out.println(s);
+
+		String randomString = "Voici le résultat d’un calcul :\n1+5=6";
+
+		System.out.println(randomString);
+
 	}
 }
