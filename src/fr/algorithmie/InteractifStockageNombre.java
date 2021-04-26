9c77c3c3cabee10f935b777f00f21d4029478d93
melheb.younes <melheb.younes@gmail.com>
2021-04-26 11:20:29 +0200

    Correction exos Fabrique mur et InteractifStockage

--- src/fr/algorithmie/InteractifStockageNombre.java
+++ src/fr/algorithmie/InteractifStockageNombre.java
@@ -3,36 +3,40 @@ package fr.algorithmie;
 import java.util.Arrays;
 import java.util.Scanner;
 
-// Exo pas fini
 public class InteractifStockageNombre {
     public static void main(String[] args) {
-        int choice = 0;
-        int number = 0;
-        int[] array = new int[4];
+        // TODO Auto-generated method stub
+        Scanner scanner = new Scanner(System.in);
+        int[] tab = new int[0];
+        int count = 0;
+        boolean stop = false;
+        while (!stop) {
+            System.out.println(
+                    "Veuillez sélectionner : \n 1. Ajouter un nombre \n 2. Afficher les nombres existants \n 3. Arrêter");
+            int choix = scanner.nextInt();
+            switch (choix) {
+                case 1:
+                    System.out.println("Vous avez choisi d'ajouter un nombre, veuillez renseigner votre nombre :");
+                    int nouveauNombre = scanner.nextInt();
+                    tab = Arrays.copyOf(tab, tab.length +1);
+                    tab[count] = nouveauNombre;
+                    count++;
+                    break;
 
-        Scanner scan = new Scanner(System.in);
+                case 2:
+                    System.out.println("Vous avez choisi d'afficher le tableau :");
+                    System.out.println(Arrays.toString(tab));
+                    break;
 
-        do {
-            System.out.println("--Menu--");
-            System.out.println("1. Ajouter un nombre : tapez 1");
-            System.out.println("2. Afficher les nombres existants : tapez 2");
-            choice = scan.nextInt();
-        } while (choice != 2 && choice != 1);
+                case 3:
+                    stop = true;
+                    System.out.println("Vous avez arrêté le programme");
+                    break;
 
-        switch (choice) {
-            case 1:
-                System.out.print("Enter un nombre : ");
-                number = scan.nextInt();
-                array[0] = number;
-                System.out.println("Voulez vous afficher le tableau ? Tapez 2");
-                choice = scan.nextInt();
-                break;
-            case 2:
-                System.out.println("Voici le tableau : ");
-                System.out.println(Arrays.toString(array));
-                break;
+                default:
+                    System.out.println(
+                            "Oups, vous n'avez pas rentré un chiffre valide...");
+            }
         }
-
-
     }
 }
